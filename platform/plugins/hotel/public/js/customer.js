dc8bd5c4526d0dcecdc9ca7c5962b72b6decb2d4
felobesabd <felobesabddd@gmail.com>
2025-12-28 20:37:19 +0200

    run build

--- platform/plugins/hotel/public/js/customer.js
+++ platform/plugins/hotel/public/js/customer.js
@@ -1,16 +1,1 @@
-/******/ (() => { // webpackBootstrap
-/*!****************************************************************!*\
-  !*** ./platform/plugins/hotel/resources/assets/js/customer.js ***!
-  \****************************************************************/
-$(document).ready(function () {
-  $(document).on('click', '#is_change_password', function (event) {
-    console.log(5);
-    if ($(event.currentTarget).is(':checked')) {
-      $('input[type=password]').closest('.form-group').removeClass('hidden').fadeIn();
-    } else {
-      $('input[type=password]').closest('.form-group').addClass('hidden').fadeOut();
-    }
-  });
-});
-/******/ })()
-;
+$(document).ready(function(){$(document).on("click","#is_change_password",function(e){console.log(5),$(e.currentTarget).is(":checked")?$("input[type=password]").closest(".form-group").removeClass("hidden").fadeIn():$("input[type=password]").closest(".form-group").addClass("hidden").fadeOut()})});
